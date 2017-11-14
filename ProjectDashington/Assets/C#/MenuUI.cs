f7f5628c33303a32f60a08edad1dc27fd971749b
c6tonsun <toni.sundell@cs.tamk.fi>
2017-11-14 13:29:28 +0200

    mid commit Menu test.

---
+++ ProjectDashington/Assets/C#/MenuUI.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuUI : MonoBehaviour {
+
+    private GameManager _gameManager;
+    private MenuHeader[] _menuHeaders;
+
+    private void Start()
+    {
+        _gameManager = FindObjectOfType<GameManager>();
+
+        _menuHeaders = transform.GetComponentsInChildren<MenuHeader>(true);
+
+        UpdateMenu();
+    }
+    
+    public void UpdateMenu()
+    {
+        foreach (MenuHeader menuHeader in _menuHeaders)
+        {
+            menuHeader.gameObject.SetActive(false);
+        }
+
+        foreach (MenuHeader menuHeader in _menuHeaders)
+        {
+            if (menuHeader.gameObject.name.Contains(_gameManager.menuMode))
+            {
+                menuHeader.gameObject.SetActive(true);
+            }
+        }
+    }
+}
