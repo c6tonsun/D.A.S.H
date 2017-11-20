2f9a1a058438400227ae870455f93f4854797c0c
Cagez123 <kalle.vikstrom@cs.tamk.fi>
2017-11-20 13:25:53 +0200

    Added end screen to world 1.

---
+++ ProjectDashington/Assets/C#/flipImage.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class flipImage : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		transform.localScale = new Vector3 (-1, 1, 1);
+	}
+}
