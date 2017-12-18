2e528b4ec03b7285a43793fe1cb9b6510adc029b
c6tonsun <toni.sundell@cs.tamk.fi>
2017-12-18 13:02:45 +0200

    Prity

---
+++ ProjectDashington/Assets/C#/StarParticle.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StarParticle : MonoBehaviour {
+
+    public ParticleSystem star;
+
+    private void OnEnable()
+    {
+        star.Play();
+    }
+}
